e52358d36c99af1ac9807d64385d2819b413fffd
sunilwritings <93305919+sunilwritings@users.noreply.github.com>
2022-12-22 07:50:07 +0530

    Update defect_detection_app.py

--- defect_detection_app.py
+++ defect_detection_app.py
@@ -28,11 +28,10 @@ from PIL import Image
 from tensorflow.keras import backend as K
 from tensorflow.keras.models import Model,load_model
 from tensorflow.keras.regularizers import l2
+%env SM_FRAMEWORK=tf.keras
 import segmentation_models as sm
 from segmentation_models import get_preprocessing
 import imgaug.augmenters as iaa
-sm.set_framework('tf.keras')
-sm.framework()
 import segmentation_models as sm
 from segmentation_models import get_preprocessing
 import streamlit as st
